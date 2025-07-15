60bef4a3ece1d66941fee26970c7daed376ef589
hrishabh1416 <hrishabhshrivastava16@gmail.com>
2025-07-15 21:31:39 +0530

    Update database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,9 +1,7 @@
 import mongoose from "mongoose";
 export const connectDB=async function() {
-    // const MONGODBURL="mongodb://127.0.0.1:27017/fooddelivery";
     const DBURL=process.env.ATLASDB_URL;
     try{
-        // await mongoose.connect(MONGODBURL);
         await mongoose.connect(DBURL);
         console.log("Connected to the DB");
     }
