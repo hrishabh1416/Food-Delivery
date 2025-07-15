107ba346538df7833428bff5c8e1b7e8da477e14
hrishabh1416 <hrishabhshrivastava16@gmail.com>
2025-07-15 20:53:47 +0530

    Update Foodcontextprovider.jsx

--- frontend/src/context/Foodcontextprovider.jsx
+++ frontend/src/context/Foodcontextprovider.jsx
@@ -7,7 +7,7 @@ function Foodcontextprovider({children}) {
   const[cartitems,setcartitems]=useState({});
   const [token,settoken]=useState("");
   const [userId, setUserId] = useState("");
-  const url='http://localhost:4000'
+  const url='https://food-delivery-vv04.onrender.com'
   const addtocart= async (itemid)=>{
     itemid = itemid.toString();
     if(!cartitems[itemid]) {
