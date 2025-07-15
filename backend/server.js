f4e0d2bd3c540c0cf29c526fcc08a5754698fcb7
hrishabh1416 <hrishabhshrivastava16@gmail.com>
2025-07-15 21:10:48 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 //   credentials: true
 // }));
 app.use(cors({
-  origin: ['http://localhost:5173','http://localhost:5174'],
+  origin: ['http://localhost:5173','http://localhost:5174','https://food-delivery-website-a3w4.onrender.com'],
   credentials: true
 }));
 
