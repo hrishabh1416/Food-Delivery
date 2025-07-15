f4ca2cd4e812c3af1c3e86f00dacea61a90bcc97
hrishabh1416 <hrishabhshrivastava16@gmail.com>
2025-07-15 22:50:09 +0530

    Update database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,11 +1,11 @@
-import mongoose from "mongoose";
-export const connectDB=async function() {
-    const DBURL=process.env.ATLASDB_URL;
-    try{
-        await mongoose.connect(DBURL);
-        console.log("Connected to the DB");
-    }
-    catch(err) {
-        console.log(err);
-    } 
-}
+export const connectDB = async () => {
+  const DBURL = process.env.ATLASDB_URL;
+  console.log("Connecting to:", DBURL);  // Add this line
+  try {
+    await mongoose.connect(DBURL);
+    console.log("Connected to the DB");
+  } catch (err) {
+    console.error("MongoDB connection failed:", err);
+  }
+};
+
